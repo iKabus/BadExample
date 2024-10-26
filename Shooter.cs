bd55c2a44e79e3350a654a799bcb88894223393c
Nikolay Semenov <164047031+iKabus@users.noreply.github.com>
2024-10-26 18:46:15 +0300

    Third

---
+++ Shooter.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private Rigibody _prefab;
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _cooldown;
+
+    private void Start()
+    {
+        StartCoroutine(ShootingWorker());
+    }
+
+    private IEnumerator ShootingWorker()
+    {
+        WaitForSecondsRealtime waitingTime = new WaitForSecondsRealtime(_cooldown);
+
+        while (enabled)
+        {
+            yield return waitingTime;
+
+            Vector3 direction = (_target.position - transform.position).normalized;
+            Rigidbody newBullet = Instantiate(_prefab, transform.position + direction, Quaternion.identity);
+
+            newBullet.transform.up = direction;
+            newBullet.velocity = direction * _speed;
+        }
+    }
+}
